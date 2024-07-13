11f04559f74fe94408adde5daec9d8d21207b2ae
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-13 20:16:03 +0100

    my tiny quiz app

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    @IBOutlet weak var imageView: UIImageView!
+    // define the radnom index
+    
+    
+    let ballArray = ["ball1","ball2","ball3", "ball4", "ball5"]
+    
+    
+    
+    @IBAction func askButton(_ sender: Any) {
+        
+        let randomIndex = Int.random(in:0...4)
+        imageView.image = UIImage(named: ballArray[randomIndex])
+        print(randomIndex)
+        print(ballArray[randomIndex])
+    }
+    
 
 
 }
